2b7904c8aceb8867ae6b86415dee60fa72b88b23
clintonraj1924 <clintonraj07@outlook.com>
2024-12-02 11:43:18 +0530

    JUnit is convert into testng

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,8 @@
 package runner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/features", 
 		glue = { "stepdefinitions", "hooks" }, 
@@ -18,6 +15,6 @@ import org.junit.runner.RunWith;
 		publish=true) // tags = "@smoke and not
 
 																								// @nocredentials"
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
